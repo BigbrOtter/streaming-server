c6c4fe5d11b55371a295ea1a077557f47f84b4de
Rick Lambrechts <rick@lambrechts.nu>
2018-06-11 10:09:31 +0200

    'Init'

---
+++ test/test-spec.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var calculator = require('../index');
+describe('calculator', function() {
+    describe('add function', function() {
+        it('adds test', function () {
+            assert.equal(true, true);
+        });
+    });
+});
