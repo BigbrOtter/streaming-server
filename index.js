c977807c35ed997e93c72bf10ac9206e83071983
Rick Lambrechts <rick@lambrechts.nu>
2018-06-20 10:31:36 +0200

    Changed index to updated index with ffmpeg

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-const NodeMediaServer = require('node-media-server')
+const NodeMediaServer = require('node-media-server');
 
 const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 60,
-    ping_timeout: 30
-  },
-  http: {
-    port: 8000,
-    allow_origin: '*'
-  }
-}
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 60,
+        ping_timeout: 30
+    },
+    http: {
+        port: 8000,
+        allow_origin: '*'
+    },
+    trans: {
+        ffmpeg: 'C:\\ffmpeg\\bin\\ffmpeg.exe',
+        tasks: [
+            {
+                app: 'live',
+                ac: 'aac',
+                hls: true,
+                hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+                dash: true,
+                dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+            }
+        ]
+    }
+};
 
-var nms = new NodeMediaServer(config)
-nms.run()
+var nms = new NodeMediaServer(config);
+nms.run();
