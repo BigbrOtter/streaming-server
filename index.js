7d0de0745d79d03d732c31bb87b17ed33bed74f7
Rick Lambrechts <rick@lambrechts.nu>
2018-06-20 14:44:49 +0200

    Added certificates Added filewatcher

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const NodeMediaServer = require('node-media-server');
+const chokidar = require('chokidar');
+const NodeRSA = require('node-rsa');
+const crypto = require('crypto');
+const fs = require('fs');
 
 const config = {
     rtmp: {
@@ -16,16 +20,55 @@ const config = {
         ffmpeg: '/usr/bin/ffmpeg',
         tasks: [
             {
+                app: 'vod',
+                ac: 'aac',
+                mp4: true,
+                mp4Flags: '[movflags=faststart]',
+            },
+            {
                 app: 'live',
                 ac: 'aac',
                 hls: true,
-                hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-                dash: true,
-                dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+                hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]'
             }
         ]
     }
 };
+
+var watcher = chokidar.watch('./media/live', {ignored: /(.+\.m3u8)|(.+\.ehash)/, persistent: true, depth: 3});
+
+watcher
+    .on('add', (path) => {
+        // console.log('File', path, 'has been added');
+    })
+    .on('change', (path) => {
+        console.log('File', path, 'has been changed');
+        const hash = crypto.createHash('sha256');
+        const input = fs.createReadStream(path);
+        input.on('readable', () => {
+            const data = input.read();
+            if (data)
+                hash.update(data);
+            else {
+                let stringhash = hash.digest('hex');
+                console.log(`${stringhash} ${path}`);
+                privateKey = fs.readFileSync(`./certificate/private.pem`, {encoding: 'utf-8'});
+                const objectPrivatePem = new NodeRSA(privateKey);
+                const encrypted = objectPrivatePem.encryptPrivate(hash, 'base64');
+                console.log(`${encrypted} ${path}`);
+                fs.writeFile(path + ".ehash", encrypted, function(err) {
+                    if(err) {
+                        return console.log(err);
+                    }
+                    console.log(`The hashfile was saved! ${path + '.ehash'}`);
+                });
+            }
+        });
+    })
+    .on('unlink', (path) => {
+        // console.log('File', path, 'has been removed');
+    })
+    .on('error', (error) => {console.error('Error happened', error);});
 
 var nms = new NodeMediaServer(config);
 nms.run();
